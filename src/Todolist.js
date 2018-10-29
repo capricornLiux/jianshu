762a1a5047be32270bfc9778b2c85fddd7d6a9ce
liuxin <liux_0118@163.com>
2018-10-29 16:33:55 +0800

    3-4

---
+++ src/Todolist.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import './style.css'
+/* 替代包裹div, 不会再dom中渲染 */
+const Fragment = React.Fragment
+class Todolist extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            inputValue: '',
+            list: []
+        }
+    }
+    render () {
+        return (
+            <Fragment>
+                <div>
+                    <label htmlFor="textArea">输入内容</label>
+                    <input type="text" 
+                        value={this.state.inputValue}
+                        onChange={this.handleInputChange.bind(this)}
+                        className="input"
+                        id="textArea"
+                    />
+                    <button onClick={this.handleSubmit.bind(this)}>提交</button>
+                </div>
+                <ul>
+                    {
+                        this.state.list.map((currentValue, index, arr) => {
+                            return <li 
+                                        key={index} 
+                                        onClick={this.handleItemClick.bind(this, index)}
+                                        dangerouslySetInnerHTML={{__html: currentValue}}
+                                    >
+                                    </li>
+                        })
+                        /* react循环渲染的时候, 渲染出的每一项需要添加一个key值, 实际编程中不要用index做key值 */
+                        /* 这个key最终不会渲染到DOM中 */
+                    }
+                </ul>
+            </Fragment>
+        )
+    }
+    handleInputChange(e) {
+        // console.log(this) // undefined
+        // this.state.inputValue = e.target.value
+        this.setState({
+            inputValue: e.target.value
+        })
+    }
+
+    // 点击提交按钮的时候调用
+    handleSubmit (e) {
+        // 点击按钮的时候
+        this.setState({
+            list: [...this.state.list, this.state.inputValue],
+            inputValue: ''
+        })
+        // 使用拓展运算符拓展list
+    }
+
+    // 点击item的时候删除这个item
+    handleItemClick (index, e) {
+        /* console.log(e.target)
+        console.log(index) */
+        // 从数组中删除
+        const list = [...this.state.list] // copy list
+        list.splice(index, 1) // splice方法改变list
+        this.setState({
+            list: list
+        })
+        
+        // react中immutable的概念, state不允许做任何改变, 修改变量可以修改变量的副本, 而不是直接操作变量
+        // 违反这个规则的时候回对react的优化产生影响
+    }
+}
+
+export default Todolist
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+/* 体现了React all in js理念 */
+/* import './index.css'; */
+import Todolist from './Todolist';
+
+ReactDOM.render(<Todolist />, document.getElementById('root'));
