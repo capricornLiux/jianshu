c9052d7969ebe21aade9d27bd2bba360debeb6f2
liuxin <liux_0118@163.com>
2018-11-01 11:35:27 +0800

    propType

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,19 +1,35 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 class TodoItem extends React.Component {
     constructor(props) {
         super(props)
         this.handleClick = this.handleClick.bind(this)
     }
     render() {
-        const { content } = this.props
+        const { content, test } = this.props
         return (
-          <div onClick={this.handleClick}>{content}</div>
+            <div onClick={this.handleClick}>
+                {test}-{content}
+            </div>
         )
     }
     handleClick() {
         const {deleteItem, index } = this.props
         deleteItem(index)
     }
+}
+
+/* 属性校验 */
+TodoItem.propTypes = {
+    test: PropTypes.string.isRequired,
+    content: PropTypes.string,
+    deleteItem: PropTypes.func,
+    index: PropTypes.number
+}
+
+/* 默认值 */
+TodoItem.defaultProps = {
+    test: 'hello world'
 }
 
 export default TodoItem
